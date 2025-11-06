f9616c892e64ba195c62986616432d59122da96e
Ali153 <132358407+Aliabdrabo@users.noreply.github.com>
2025-11-06 17:51:52 +0200

    Add branch name parameter to deployment function

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -1,8 +1,9 @@
-def call(String deploymentFile, String kubeconfigCredId ) {
+def call(String deploymentFile, String kubeconfigCredId, String branchName ) {
     
     withCredentials([file(credentialsId: kubeconfigCredId , variable: 'KUBECONFIG')]) {
         
 	echo "___deploy-function___"
-	sh "kubectl --kubeconfig=$KUBECONFIG apply -f ${deploymentFile}"
+	sh "sed -i 's|image:.*|image: ${IMAGE_NAME}:v${BUILD_NUMBER}|' ${deploymentFile}"	
+	sh "kubectl --kubeconfig=$KUBECONFIG apply -f ${deploymentFile} -n ${branchName} "
     }
 }
